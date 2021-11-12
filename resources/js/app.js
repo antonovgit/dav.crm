096978346fa197b9ae5eeca25e46a14eafc00051
Dmitrii Antonov <d.antonovbox@gmail.com>
2021-11-12 20:57:44 +0200

    Laravel mix, VueJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+
+console.log('is running')
+
+//Добавляется(регистрируется) комонент. Первый параметр 'home' - это имя компонента, второй - путь к файлу данного компонента
+Vue.component('home', require('./components/Home.vue').default);
+
+const app = new Vue({
+    el: "#app"
+});
